d2f00ef108c78ad37593488aa7f67f3dd4608568
Hope-Kathomi <hopekathomi@gmail.com>
2026-09-04 15:44:09 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useState, createContext } from 'react'
 import Landingpage from './components/Landingpage'
 import Nav from './components/common/Nav'
 import ProductList from './components/ProductList'
 import Administrator from './components/Administrator'
+
+export const userContext = createContext(null);
 
 function App() {
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <userProvide>
+      <App />
+    </userProvide>
   </StrictMode>,
 )
