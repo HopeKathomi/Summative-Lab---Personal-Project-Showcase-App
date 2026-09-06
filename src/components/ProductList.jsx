79210cf5b3e424b225fc4671cd60ea3d8c1429c5
Hope-Kathomi <hopekathomi@gmail.com>
2026-09-06 21:18:23 +0300

    all functionality done

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,27 @@
 import Product from './Product';
+import Search from './Search';
+import { useState, createContext } from 'react';
 
+export const SearchUserContext = createContext();
 const ProductList = ({coffees}) => {
- 
+
+  const [searchWord, setSearchWord] = useState("");
+
+
+  function handleSearch(e){
+    e.preventDefault();
+
+  }
   return (
-    <Product coffees = {coffees}/>
+    <div className='bg-amber-100 flex flex-row flex-wrap justify-center items-center gap-7 py-20'>
+      <SearchUserContext value={{searchWord, setSearchWord}}>
+        <Search/>
+        <Product coffees = {coffees}/>
+      </SearchUserContext>
+
+      
+    </div>
+    
   )
 }
 
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { useContext } from 'react'
+import { SearchUserContext } from '../components/ProductList'
 
 const Search = () => {
+  const {searchWord, setSearchWord} = useContext(SearchUserContext);
+  console.log("serch", searchWord);
+
+  function handleChange(e){
+    e.preventDefault();
+    setSearchWord(e.target.value);
+  }
   return (
-    <div>Search</div>
+    <div>Search
+      <input type="search" value={searchWord} onChange={handleChange}className='bg-white border border-slate-200 rounded-xl py-2 px-3' />
+    </div>
   )
 }
 
